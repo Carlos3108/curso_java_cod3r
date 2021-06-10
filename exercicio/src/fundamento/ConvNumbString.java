656c08353ad239367bbcc529cbd84147b8504606
Carlos3108 <c.silva1609@gmail.com>
2021-06-09 23:44:29 -0300

    Fim da aula de Conversao de Numero para String

--- exercicio/src/fundamento/ConvNumbString.java
+++ exercicio/src/fundamento/ConvNumbString.java
@@ -3,6 +3,9 @@ package fundamento;
 public class ConvNumbString {
     public static void main(String[] args) {
         Integer num1 = 1000;
-        System.out.println(num1);
+        System.out.println(num1.toString());
+
+        int num2 = 10000;
+        System.out.println(Integer.toString(num2));
     }
 }
