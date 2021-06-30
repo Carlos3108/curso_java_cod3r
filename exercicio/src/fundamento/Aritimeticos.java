ff4cb464cfbb15ffeaaca41a97d268db675532aa
Carlos3108 <c.silva1609@gmail.com>
2021-06-30 01:11:35 -0300

    Aula sobre operacoes aritimeticas

---
+++ exercicio/src/fundamento/Aritimeticos.java
@@ -0,0 +1,24 @@
+package fundamento;
+
+public class Aritimeticos {
+    public static void main(String[] args) {
+        System.out.println(3 + 3);
+
+        var x = 3.5;
+        var y = 5;
+
+        System.out.println(x + y);
+        System.out.println(x - y);
+        System.out.println(x * y);
+        System.out.println(x / y);
+
+        var a = 8;
+        var b = 3;
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / (double) b); //ou float
+        System.out.println(a % b); //resto da divisão
+    }
+}
