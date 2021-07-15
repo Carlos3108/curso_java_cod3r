f4d994689cd6a2602ac616571b0cc9d40f412c00
Carlos Silva <c.silva1609@gmail.com>
2021-07-15 01:08:57 -0300

    Encerramento da aula de Igualdade com String

--- exercicio/src/fundamento/TipoStringEquals.java
+++ exercicio/src/fundamento/TipoStringEquals.java
@@ -11,10 +11,12 @@ public class TipoStringEquals {
         System.out.println("2".equals(s)); //equals faz a comparação do conteudo;
 
         Scanner entrada = new Scanner(System.in);
-        String s2 = entrada.next();
+        String s2 = entrada.nextLine(); //next remove as linhas;
         System.out.println("2" == s2.trim());
-        System.out.println("2".equals(s2.trim()));
+        System.out.println("2".equals(s2.trim())); //.trim remove os espaços em brancos;
 
         entrada.close();
+
+        //Sempre comparar Strings utilizando o '.equals()';
     }
 }
