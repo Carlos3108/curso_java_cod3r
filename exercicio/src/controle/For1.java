fbe445473c061f288500252a25526cc5462935d4
Carlos Silva <c.silva1609@gmail.com>
2021-07-31 22:19:04 -0300

    Aulas de desafio If e Else. Aula de while.

---
+++ exercicio/src/controle/For1.java
@@ -0,0 +1,10 @@
+package controle;
+
+public class For1 {
+    public static void main(String[] args) {
+        for(int contador = 0; contador <=20; contador += 2) {
+            System.out.printf("i = %d \n", contador);
+        }
+    }
+}
+//1º declarar a variavel, 2º expressão que determina se é verdadeiro ou falso e 3º manipulação da variavel.
