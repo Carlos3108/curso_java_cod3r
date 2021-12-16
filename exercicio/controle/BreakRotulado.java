98623cbf195f5a6e8e4379bc27cd99281657f175
Carlos3108 <c.silva1609@gmail.com>
2021-12-15 23:53:14 -0300

    Correção do for interno

--- exercicio/controle/BreakRotulado.java
+++ exercicio/controle/BreakRotulado.java
@@ -4,7 +4,7 @@ public class BreakRotulado {
     public static void main(String[] args) {
         externo:
         for (int i = 0; i < 3; i++) {
-            for (int j = 0; j <3; i++){
+            for (int j = 0; j <3; j++){
                 if (i == 1) {
                     break externo;
                 }
--- exercicio/controle/ContinueRotulado.java
+++ exercicio/controle/ContinueRotulado.java
@@ -4,7 +4,7 @@ public class ContinueRotulado {
     public static void main(String[] args) {
         externo:
         for (int i = 0; i < 3; i++) {
-            for (int j = 0; j <3; i++){
+            for (int j = 0; j < 3; j++){
                 if (i == 1) {
                     continue externo;
                 }
