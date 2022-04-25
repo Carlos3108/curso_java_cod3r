8f0f9fb884de350060a767844f4f65f17ca3bf76
csilv <c.silva1609@gmail.com>
2022-04-25 06:44:36 -0300

    "This usado para se referir ao objeto, classe ou outra entidade da qual o código atualmente em execução faz parte."

--- exercicio/classes_metodos/exercicio_data/TesteData.java
+++ exercicio/classes_metodos/exercicio_data/TesteData.java
@@ -2,18 +2,17 @@ package classes_metodos.exercicio_data;
 
 public class TesteData {
     public static void main(String[] args) {
-        Data d1 = new Data(31, 8, 2000);
+        Data d1 = new Data();
+        d1.ano = 2001;
 
-        var d2 = new Data();
-        d2.dia = 8;
-        d2.mes = 9;
-        d2.ano = 2000;
+        var d2 = new Data(31, 8, 2000);
 
-        Data d3 = new Data();
+        String dataFormatada1 = d1.obterData();
 
-        System.out.println(d1.obterData());
+        System.out.println(dataFormatada1);
         System.out.println(d2.obterData());
-        System.out.println(d3.obterData());
 
+        d1.imprimirDataFormatada();
+        d2.imprimirDataFormatada();
     }
 }
--- exercicio/classes_metodos/exercicio_data/ValorVsReferencia.java
+++ exercicio/classes_metodos/exercicio_data/ValorVsReferencia.java
@@ -1,7 +1,5 @@
 package classes_metodos.exercicio_data;
 
-import classes_metodos.exercicio_data.Data;
-
 public class ValorVsReferencia {
     public static void main(String[] args) {
         double a = 2;
