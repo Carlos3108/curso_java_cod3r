a0b92ffdb31d6719394f139ef5571c037f56d804
CarlosSilva <c.silva1609@gmail.com>
2022-09-05 20:38:51 -0300

    "Rrelacionamento 1 para muitos"

--- src/oo/composicao/Compra.java
+++ src/oo/composicao/Compra.java
@@ -6,5 +6,18 @@ public class Compra {
     String cliente;
     ArrayList<Item> itens = new ArrayList<>();
 
-    double getValorTotal
+    void adicionarItem(String nome, int quantidade, double preco){
+        this.adicionarItem(new Item(nome, quantidade, preco));
+    }
+    void adicionarItem(Item item){
+        itens.add(item);
+        item.compra = this;
+    }
+    double getValorTotal(){
+        double total = 0;
+        for (Item item: itens){
+            total += item.quantidade * item.preco;
+        }
+        return total;
+    }
 }
--- src/oo/composicao/CompraTeste.java
+++ src/oo/composicao/CompraTeste.java
@@ -4,10 +4,11 @@ public class CompraTeste {
     public static void main(String[] args) {
         Compra compra1 = new Compra();
         compra1.cliente = "Carlos Silva";
-        compra1.itens.add(new Item("Caneta", 20, 7.5));
-        compra1.itens.add(new Item("Borracha", 12, 3.9));
-        compra1.itens.add(new Item("Caderno", 3, 18));
+        compra1.adicionarItem(new Item("Caneta", 20, 7.5));
+        compra1.adicionarItem(new Item("Borracha", 12, 3.9));
+        compra1.adicionarItem(new Item("Caderno", 3, 18));
 
         System.out.println(compra1.itens.size());
+        System.out.println(compra1.getValorTotal());
     }
 }
